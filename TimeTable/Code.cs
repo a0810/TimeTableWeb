57c27a98c1a20f9d4181e557ad802fe8f7fe2832
Johnathon Millard <JM2073@student.aru.ac.uk>
2023-10-05 03:11:12 +0100

    added code to search a lecture array and return a new array of results.
    DEV NOTE: at current this searches based on most fields of a Lecture, a possible improvement could be to including checkbox filters that would easily be passed though as Bool variables with a default of FALSE, as of now this is O.O.S

---
+++ TimeTable/Code.cs
@@ -0,0 +1,31 @@
+using TimeTable.Models;
+
+namespace TimeTable;
+
+/// <summary>
+/// This class is for code that will assist functionality within the application 
+/// </summary>
+public class Code
+{
+
+    /// <summary>
+    /// calling this method will search all possible fields and return a list of all matching results.   
+    /// </summary>
+    /// <param name="lectures">pass though the oringal array of lectures</param>
+    /// <param name="searchTerm">the term that is being searched by</param>
+    /// <returns>a list of results</returns>
+    public Lecture[]? SearchLectures(Lecture[] lectures, string searchTerm)
+    {
+        Lecture[] result = new Lecture[]{};
+
+        result = lectures.Where(x =>
+            x.courseName.Contains(searchTerm)  ||
+            x.moduleName.Contains(searchTerm) ||
+            x.lecturerName.Contains(searchTerm) ||
+            x.roomNumber.ToString().Contains(searchTerm)||
+            x.moduleCode.Contains(searchTerm)).ToArray();
+        
+        return result;
+    }
+    
+}
