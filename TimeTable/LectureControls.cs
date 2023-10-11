9f15f61378db14be31883018d438efb60aa5d77b
Johnathon Millard <JM2073@student.aru.ac.uk>
2023-10-11 02:19:56 +0100

    OW BOI
    deleted Code.cs and moved contence to LectureControls.cs
    
    added debugging data to Lecture.json
    
    Home.razor now populates with the data from Lecture.json

--- TimeTable/LectureControls.cs
+++ TimeTable/LectureControls.cs
@@ -2,7 +2,6 @@
 using Newtonsoft.Json.Linq;
 using System.Text.Json;
 using TimeTable.Models;
-using static TimeTable.Pages.FetchData;
 
 namespace TimeTable
 {
@@ -45,7 +44,6 @@ namespace TimeTable
 
             foreach (Lecture json in existingJson)
             {
-
                 if (json.ID == ID)
                 {
                     json.courseName = CourseName;
@@ -83,6 +81,38 @@ namespace TimeTable
             string updatedJson = JsonConvert.SerializeObject(existingJson,Formatting.Indented);
 
             File.WriteAllText(doc, updatedJson);
+
+        }
+        
+        /// <summary>
+        /// calling this method will search all possible fields and return a list of all matching results.   
+        /// </summary>
+        /// <param name="lectures">pass though the oringal array of lectures</param>
+        /// <param name="searchTerm">the term that is being searched by</param>
+        /// <returns>a list of results</returns>
+        public Lecture[]? SearchLectures(Lecture[] lectures, string searchTerm)
+        {
+            Lecture[] result = new Lecture[]{};
+
+            result = lectures.Where(x =>
+                x.courseName.Contains(searchTerm)  ||
+                x.moduleName.Contains(searchTerm) ||
+                x.lecturerName.Contains(searchTerm) ||
+                x.roomNumber.ToString().Contains(searchTerm)||
+                x.moduleCode.Contains(searchTerm)).ToArray();
+        
+            return result;
+        }
+    
+
+        /// <summary>
+        /// calling this method will create a new GUID and convert it into a string so that i can be used as a unique identifier 
+        /// </summary>
+        /// <returns>a string GUID</returns>
+        public string NewID()
+        {
+            return Guid.NewGuid().ToString();
+
 
         }
     }
